c974176a3bd90324b278a71201cf70bc0a214dd3
Kompulsif <75795732+kompulsif@users.noreply.github.com>
2025-01-27 23:50:10 +0300

    pytest models

--- models.py
+++ models.py
@@ -4,62 +4,62 @@ from pydantic import BaseModel
 
 
 class Station(BaseModel):
-    distance: int
-    latitude: float
-    longitude: float
-    useCount: int
-    id: str
-    name: str
-    quality: int
-    contribution: int
+    distance: Optional[int]
+    latitude: Optional[float]
+    longitude: Optional[float]
+    useCount: Optional[int]
+    id: Optional[str]
+    name: Optional[str]
+    quality: Optional[int]
+    contribution: Optional[int | float]
 
 
 class Day(BaseModel):
-    datetime: str
-    datetimeEpoch: int
-    tempmax: float
-    tempmin: float
-    temp: float
-    feelslikemax: float
-    feelslikemin: float
-    feelslike: float
-    dew: float
-    humidity: float
+    datetime: Optional[str]
+    datetimeEpoch: Optional[int]
+    tempmax: Optional[float]
+    tempmin: Optional[float]
+    temp: Optional[float]
+    feelslikemax: Optional[float]
+    feelslikemin: Optional[float]
+    feelslike: Optional[float]
+    dew: Optional[float]
+    humidity: Optional[float]
     precip: Optional[float]
     precipprob: Optional[int | float]
-    precipcover: float
+    precipcover: Optional[float]
     preciptype: Optional[List[str]]
     snow: Optional[float]
     snowdepth: Optional[float]
-    windgust: float
-    windspeed: float
-    winddir: float
-    pressure: float
-    cloudcover: float
-    visibility: float
-    solarradiation: float
-    solarenergy: float
-    uvindex: int
-    severerisk: int
+    windgust: Optional[float]
+    windspeed: Optional[float]
+    winddir: Optional[float]
+    pressure: Optional[float]
+    cloudcover: Optional[float]
+    visibility: Optional[float]
+    solarradiation: Optional[float]
+    solarenergy: Optional[float]
+    uvindex: Optional[int]
+    severerisk: Optional[int]
     sunrise: Optional[str]
     sunriseEpoch: Optional[int]
     sunset: Optional[str]
     sunsetEpoch: Optional[int]
-    moonphase: float
-    conditions: str
-    description: str
-    icon: str
-    stations: List[str]
-    source: str
+    moonphase: Optional[float]
+    conditions: Optional[str]
+    description: Optional[str]
+    icon: Optional[str]
+    stations: Optional[List[str]]
+    source: Optional[str]
 
 
 class WeatherResponse(BaseModel):
-    queryCost: int
-    latitude: float
-    longitude: float
-    resolvedAddress: str
-    address: str
-    timezone: str
-    tzoffset: int
+    queryCost: Optional[int]
+    latitude: Optional[float]
+    longitude: Optional[float]
+    resolvedAddress: Optional[str]
+    address: Optional[str]
+    timezone: Optional[str]
+    tzoffset: Optional[int]
     days: List[Day] = []
     stations: Dict[str, Station] = {}
---
+++ tests/test_models.py
@@ -0,0 +1,150 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from typing import Any, Dict
+
+import pytest
+
+from models import Day, Station, WeatherResponse
+
+
+@pytest.fixture
+def getStationData() -> Dict[str, Any]:
+    return {
+        "distance": 22252.0,
+        "latitude": 39.9272,
+        "longitude": 32.8644,
+        "useCount": 0,
+        "id": "LTAC",
+        "name": "LTAC",
+        "quality": 50,
+        "contribution": 0.0
+    }
+
+@pytest.fixture
+def getDayData() -> Dict[str, Any]:
+    return {
+      "datetime": "2025-01-27",
+      "datetimeEpoch": 1737925200,
+      "tempmax": 45.5,
+      "tempmin": 27.8,
+      "temp": 34.4,
+      "feelslikemax": 42,
+      "feelslikemin": 27.8,
+      "feelslike": 33.4,
+      "dew": 31.7,
+      "humidity": 90.6,
+      "precip": 0,
+      "precipprob": 0,
+      "precipcover": 0,
+      "preciptype": None,
+      "snow": 0,
+      "snowdepth": 0,
+      "windgust": 7.8,
+      "windspeed": 6.9,
+      "winddir": 193.1,
+      "pressure": 1024.1,
+      "cloudcover": 77.9,
+      "visibility": 4.3,
+      "solarradiation": 135.7,
+      "solarenergy": 11.6,
+      "uvindex": 5,
+      "severerisk": 10,
+      "sunrise": "08:01:16",
+      "sunriseEpoch": 1737954076,
+      "sunset": "18:01:55",
+      "sunsetEpoch": 1737990115,
+      "moonphase": 0.93,
+      "conditions": "Partially cloudy",
+      "description": "Partly cloudy throughout the day.",
+      "icon": "partly-cloudy-day",
+      "stations": [
+        "LTAC",
+        "LTAB",
+        "LTAE"
+      ],
+      "source": "comb"
+      }
+
+@pytest.fixture
+def getResponseData(getDayData: Dict[str, Any], getStationData: Dict[str, Any]) -> Dict[str, Any]:
+    return {
+        "queryCost": 1,
+        "latitude": 39.9272,
+        "longitude": 32.8644,
+        "resolvedAddress": "39.927200,32.864400",
+        "address": "39.927200,32.864400",
+        "timezone": "Europe/Istanbul",
+        "tzoffset": 3,
+        "days": [Day(**getDayData)],
+        "stations": {"LTAC" : Station(**getStationData)}
+    }
+
+
+def test_station_class(getStationData: Dict[str, Any]) -> None:
+    station = Station(**getStationData)
+    assert station.distance == 22252.0
+    assert station.latitude == 39.9272
+    assert station.longitude == 32.8644
+    assert station.useCount == 0
+    assert station.id == "LTAC"
+    assert station.name == "LTAC"
+    assert station.quality == 50
+    assert station.contribution == 0.0
+
+
+def test_day_class(getDayData: Dict[str, Any]) -> None:
+    
+    day = Day(**getDayData)
+    
+    assert day.datetime == "2025-01-27"
+    assert day.datetimeEpoch == 1737925200
+    assert day.tempmax == 45.5
+    assert day.tempmin == 27.8
+    assert day.temp == 34.4
+    assert day.feelslikemax == 42
+    assert day.feelslikemin == 27.8
+    assert day.feelslike == 33.4
+    assert day.dew == 31.7
+    assert day.humidity == 90.6
+    assert day.precip == 0
+    assert day.precipprob == 0
+    assert day.precipcover == 0
+    assert day.preciptype is None
+    assert day.snow == 0
+    assert day.snowdepth == 0
+    assert day.windgust == 7.8
+    assert day.windspeed == 6.9
+    assert day.winddir == 193.1
+    assert day.pressure == 1024.1
+    assert day.cloudcover == 77.9
+    assert day.visibility == 4.3
+    assert day.solarradiation == 135.7
+    assert day.solarenergy == 11.6
+    assert day.uvindex == 5
+    assert day.severerisk == 10
+    assert day.sunrise == "08:01:16"
+    assert day.sunriseEpoch == 1737954076
+    assert day.sunset == "18:01:55"
+    assert day.sunsetEpoch == 1737990115
+    assert day.moonphase == 0.93
+    assert day.conditions == "Partially cloudy"
+    assert day.description == "Partly cloudy throughout the day."
+    assert day.icon == "partly-cloudy-day"
+    assert day.stations == ["LTAC", "LTAB", "LTAE"]
+    
+
+
+def test_wheatherResponse_class(getDayData: Dict[str, Any], getStationData: Dict[str, Any], getResponseData: Dict[str, Any]) -> None:
+    wres = WeatherResponse(**getResponseData)
+    assert wres.queryCost == 1
+    assert wres.latitude == 39.9272
+    assert wres.longitude == 32.8644
+    assert wres.resolvedAddress == "39.927200,32.864400"
+    assert wres.address == "39.927200,32.864400"
+    assert wres.timezone == "Europe/Istanbul"
+    assert wres.tzoffset == 3
+    assert wres.days == [Day(**getDayData)]
+    assert wres.stations == {"LTAC": Station(**getStationData)}
